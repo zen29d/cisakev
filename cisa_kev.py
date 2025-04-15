2a5c642652723b3ccac0cc226d0027180e9e1175
Zen <zen29d@gmail.com>
2025-04-16 01:17:53 +0530

    data fetching changed from csv to json

--- cisa_kev.py
+++ cisa_kev.py
@@ -1,59 +1,76 @@
 import requests
-import csv
+import json
 import os
-from io import StringIO
 from logger import init_logger
 
 # CISA KEV API Endpoint
-URL_CISA_KEV_CSV = "https://www.cisa.gov/sites/default/files/csv/known_exploited_vulnerabilities.csv"
 URL_CISA_KEV_JSON = "https://www.cisa.gov/sites/default/files/feeds/known_exploited_vulnerabilities.json"
 STORAGE_LOCATION = "local"
-FILENAME = "cisa_kev_seen.csv"
+FILENAME = "cisa_kevs_catalog.json"
 KEV_FILE = os.path.join(STORAGE_LOCATION,FILENAME)
 
 # Initialize logging
 LOG_DIR = "log"
 LOG_FILE = "cisa_kev.log"
 log = init_logger(LOG_DIR, LOG_FILE)
-# log.basicConfig(filename=LOG_FILE, level=log.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def fetch_kev_data():
     try:
-        response = requests.get(URL_CISA_KEV_CSV, timeout=10)
+        response = requests.get(URL_CISA_KEV_JSON, timeout=5)
         if response.status_code == 200:
-            csv_data = response.text
-            return list(csv.DictReader(StringIO(csv_data)))
+            data = response.json()
+            return data
         else:
             log.warning(f"Unexpected response status: {response.status_code}")
     except requests.RequestException as E:
-        log.error(f"Failed to fetch KEV data: {E}")
-    except csv.Error as E:
-        log.error(f"Error parsing CSV data: {E}")
-    return []
+        log.error(f"Failed to fetch CISA KEV data: {E}")
+    return None
 
-def save_new_kevs(kevs):
-    if not kevs:
-        return
+def transform_kevs(json_data):
+    properties = {}
+    kev_rows = []
+    if json_data:
+        properties = {
+            'title': json_data.get('title', ''),
+            'catalogVersion': json_data.get('catalogVersion', ''),
+            'dateReleased': json_data.get('dateReleased', ''),
+            'count': json_data.get('count', '')
+        }
+        kevs = json_data.get('vulnerabilities',[])
 
+        fields = list(kevs[0].keys())
+        for item in kevs:
+            row = {field: item.get(field, '') for field in fields}
+            kev_rows.append(row)
+        
+    return [properties, kev_rows]
+
+
+def save_kevs(json_data):
     try:
-        with open(KEV_FILE, "w", newline="", encoding="utf-8") as file:
-            fieldnames = kevs[0].keys()
-            writer = csv.DictWriter(file, fieldnames=fieldnames)
-            writer.writeheader()
-            writer.writerows(kevs)
-    except Exception as e:
-        log.error(f"Error saving KEV data: {e}")
+        with open(KEV_FILE, "w", encoding="utf-8") as f:
+            json.dump(json_data, f, indent=2)
+    except Exception as E:
+        log.error(f"Error saving KEV JSON data: {E}")
 
-def download_kevs():
+
+def download_kevs(is_update = False):
     try:
         os.makedirs(STORAGE_LOCATION, exist_ok=True)
-    except Exception as e:
-        log.error(f"Failed to create file location: {e}")
+    except Exception as E:
+        log.error(f"❌ Failed to create storage directory: {E}")
+        return
 
-    log.info("Downloading CISA KEV Catalog")
-    data = fetch_kev_data()
-    save_new_kevs(data)
-    log.info(f"🚀 Saved {len(data)} new KEVs in {KEV_FILE}")
+    action = "Updating" if is_update else "Downloading"
+    log.info(f"{action} CISA KEV Catalog...")
+
+    json_data = fetch_kev_data()
+    if not json_data:
+        log.warning("No KEV data fetched from CISA")
+        return
+    
+    save_kevs(json_data)
+
 
 def load_seen_kevs():
     if not os.path.exists(KEV_FILE):
@@ -61,29 +78,21 @@ def load_seen_kevs():
         return []
     try:
         with open(KEV_FILE, "r", newline="", encoding="utf-8") as file:
-            return list(csv.DictReader(file))
-    except Exception as e:
-        log.error(f"Error loading previous KEV data: {e}")
+            return json.load(file)
+    except Exception as E:
+        log.error(f"Error loading previous KEV data: {E}")
         return []
 
 
-
-# Main function
+# Main Test function
 def main():
-    previous_kevs = load_seen_kevs()
-    if not previous_kevs:
-        download_kevs()
-        return
-    latest_kevs = fetch_kev_data()
-
-    previous_cve_ids = {kev["cveID"] for kev in previous_kevs}
-    new_kevs = [kev for kev in latest_kevs if kev["cveID"] not in previous_cve_ids]
-    if new_kevs:
-        log.info(f"🚀 Found {len(new_kevs)} new KEVs")
-        save_new_kevs(latest_kevs)
-    else:
-        log.info("✅ No new KEVs detected")
+    log.info(f"Test Start: {__name__}")
+    download_kevs()
+    log.info(f"Loading downloaded KEVs data from {KEV_FILE}")
+    json_data = load_seen_kevs()
+    properties, kevs = transform_kevs(json_data)
+    print(properties)
+    log.info(f"Test End: {__name__}")
 
 if __name__ == "__main__":
     main()
-
--- cisa_kev_watcher.py
+++ cisa_kev_watcher.py
@@ -1,28 +1,55 @@
 from cisa_kev import *
 from webhook_push import *
 from logger import init_logger
+from datetime import datetime
 
 # Initialize logging
 log = init_logger()
 
 
-def main():
-    previous_kevs = load_seen_kevs()
+def is_new_release(prev_date, latest_date):
+    try:
+        prev = datetime.fromisoformat(prev_date.replace("Z", "+00:00"))
+        latest = datetime.fromisoformat(latest_date.replace("Z", "+00:00"))
+        is_new_flag = latest > prev
+        return is_new_flag
+    except Exception as E:
+        log.error(f"Date comparison failed: {E}")
+        return False
+
+
+def check_new_kevs():
+    previous_props, previous_kevs = transform_kevs(load_seen_kevs())
     if not previous_kevs:
         download_kevs()
-        return
-    latest_kevs = fetch_kev_data()
-    webhooks = load_webhooks()
+        return None
 
-    previous_cve_ids = {kev["cveID"] for kev in previous_kevs}
-    new_kevs = [kev for kev in latest_kevs if kev["cveID"] not in previous_cve_ids]
-    
-    if new_kevs:
-        log.info(f"🚀 Found {len(new_kevs)} new KEVs")
+    latest_props, latest_kevs = transform_kevs(fetch_kev_data())
+    if not latest_kevs:
+        log.warning("⚠️ No KEV data fetched")
+        return None
+
+    is_new = is_new_release(previous_props['dateReleased'], latest_props['dateReleased'])
+    if is_new:
+        download_kevs(is_update=True)
+        previous_ids = {kev["cveID"] for kev in previous_kevs}
+        new_items = [kev for kev in latest_kevs if kev["cveID"] not in previous_ids]
+        return new_items
+
+    return []
+
+
+def main():
+    webhooks = load_webhooks()
+    new_kevs = check_new_kevs()
+    if new_kevs is None:
+        return
+    elif new_kevs:
+        log.info(f"🚨 Found {len(new_kevs)} new KEVs")
         send_notifications(new_kevs, webhooks)
-        save_new_kevs(latest_kevs)
     else:
         log.info("✅ No new KEVs detected")
+
 
 if __name__ == "__main__":
     main()
